34c13eaa46aa56f066e20e0c39c1ac9c3773d0de
rikuyaosawa <rikuyaosawa@gmail.com>
2024-12-22 20:07:55 +0900

    add comments

--- prototypes/index.js
+++ prototypes/index.js
@@ -1,3 +1,7 @@
+// Every object in JavaScript has a built-in property, which is called its prototype.
+// The prototype is itself an object, so the prototype will have its own prototype, making what's called a prototype chain.
+// The chain ends when we reach a prototype that has null for its own prototype.
+
 function Book(title, author, genre, isAvailable = true) {
     this.id = Math.floor(Math.random() * 1000); // for test
     this.title = title;
