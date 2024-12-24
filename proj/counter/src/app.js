c594356144a48366108821eb54e9a519b9dd3271
rikuyaosawa <rikuyaosawa@gmail.com>
2024-12-24 22:32:15 +0900

    final implementation

--- proj/counter/src/app.js
+++ proj/counter/src/app.js
@@ -1,29 +1,36 @@
 let count = 0;
+let currentValueColor = "#222";
 
-function decrease() {
-    count--;
-    document.getElementById("value").textContent = count;
-    updateFontColor();
-}
+let value = document.querySelector("#value");
+let btns = document.querySelectorAll(".btn");
 
-function reset() {
-    count = 0;
-    document.getElementById("value").textContent = count;
-    updateFontColor();
-}
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        let styles = e.currentTarget.classList;
+        if (styles.contains("increase")) {
+            count++;
+        } else if (styles.contains("decrease")) {
+            count--;
+        } else if (styles.contains("reset")) {
+            count = 0;
+        }
+        updateTextColorIfNecessary(count);
+        value.textContent = count;
+    });
+});
 
-function increase() {
-    count++;
-    document.getElementById("value").textContent = count;
-    updateFontColor();
-}
-
-function updateFontColor() {
-    if (count < 0) {
-        document.getElementById("value").style.color = "red";
-    } else if (0 < count) {
-        document.getElementById("value").style.color = "green";
+function updateTextColorIfNecessary(n) {
+    let newColor;
+    if (n > 0) {
+        newColor = "green";
+    } else if (n < 0) {
+        newColor = "red";
     } else {
-        document.getElementById("value").style.color = "hsl(209, 61%, 16%)";
+        newColor = "#222";
+    }
+
+    if (newColor !== currentValueColor) {
+        value.style.color = newColor;
+        currentValueColor = newColor;
     }
 }
